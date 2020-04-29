a14bb5f71a67336d4fd2c633bc83443e95011833
sllash2000 <sllash.arthur@gmail.com>
2020-04-30 00:16:32 +0700

    Finished implemented Models and registering to admin

--- Semproject/Courseoutline/admin.py
+++ Semproject/Courseoutline/admin.py
@@ -3,3 +3,9 @@ from .models import *
 # Register your models here.
 admin.site.register(CourseOutline)
 admin.site.register(Curriculum)
+admin.site.register(CourseOutlineSection1)
+admin.site.register(CourseOutlineSection2)
+admin.site.register(CourseOutlineSection3)
+admin.site.register(CourseOutlineSection4)
+admin.site.register(CourseOutlineSection5)
+admin.site.register(CourseOutlineSection6)
--- Semproject/Courseoutline/models.py
+++ Semproject/Courseoutline/models.py
@@ -9,7 +9,21 @@ FacultyNameChoices = [
     ('FAH','Arts And Humanities'),('FBA','Business Admin'),
     ('FEd','Education'),('FRS','Religous Studies'),('FOS','Science'),
     ('FIT','Information Technology'),('FON','Nursing')]
-
+CourseCategory = [
+    ('Core Course','Core Course'),('General Education Course','General Education Course'),
+    ('Major Required Course','Major Required Course'),('Major Elective Course','Major Elective Course')
+]
+HoursChoices = [
+    ('1','1'),('2','2'),('3','3'),('4','4')
+]
+ResourcesTypes = [
+    ('Text Book','Text Book'),('Internet Resources','Internet Resources'),
+    ('Research Paper','Reserach Paper')
+]
+EvaluationTypes = [
+    ('Assignments','Assignments'),('Quizzes','Quizzes'),('Attendance','Attendance'),
+    ('Midterm Exam','Midterm Exam'),('Final Exam','Final Exam'),('Projects','Projects')
+]
 class Curriculum(models.Model):
 
     CurriculumID = models.AutoField(primary_key=True,max_length=10)
@@ -29,3 +43,58 @@ class CourseOutline(models.Model):
     def __str__(self):
         return self.CourseName
 
+class CourseOutlineSection1(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Course Identification and General Information')
+    DateCreated = models.DateTimeField(default=timezone.now)
+    NumberOfCredit = models.CharField(max_length=10)
+    CategoryofCourse = models.CharField(max_length=55,choices=CourseCategory)
+    Prerequisite = models.CharField(max_length=100)
+    Corequisite = models.CharField(max_length=100)
+    Place = models.CharField(max_length=100)
+    DateofCourseDevelopmentorRevision = models.DateField()
+
+class CourseOutlineSection2(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Aims and Objectives')
+    DateCreated = models.DateTimeField(default = timezone.now)
+    CourseAims = models.TextField(max_length=555)
+    ObjofCourse = models.TextField(max_length=555)
+
+class CourseOutlineSection3(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Course Composition')
+    DateCreated = models.DateTimeField(default = timezone.now)
+    CourseDesc =  models.TextField(max_length=555)
+    NumofLectureHrs = models.IntegerField(max_length=2,choices=HoursChoices)
+    NumofAddLectureHrs = models.IntegerField(max_length=2,choices=HoursChoices)
+    NumofLabHrs = models.IntegerField(max_length=2,choices=HoursChoices)
+    NumofSSHrs = models.IntegerField(max_length=2,choices=HoursChoices)
+    NumofAdvHrs = models.IntegerField(max_length=2,choices=HoursChoices)
+
+class CourseOutlineSection4(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Course Topics')
+    DateCreated = models.DateTimeField(default = timezone.now)
+    CourseTopic = models.TextField(max_length=555)    
+    CourseTopicDesc = models.TextField(max_length=555) 
+
+class CourseOutlineSection5(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Evaluation Plans')
+    DateCreated = models.DateTimeField(default = timezone.now)
+    EvaluationType = models.CharField(max_length=20,choices=EvaluationTypes)    
+    EvaluationPercentage = models.CharField(max_length=3)    
+
+class CourseOutlineSection6(models.Model):
+    SectionID = models.AutoField(primary_key = True)
+    CourseOutlineID = models.ForeignKey(CourseOutline,on_delete=models.CASCADE)
+    SectionName = models.CharField(max_length=55,default='Resources')
+    DateCreated = models.DateTimeField(default = timezone.now)
+    ResourcesTypes = models.CharField(max_length=20, choices = ResourcesTypes)    
+    ResourcesDescription = models.TextField(max_length=3)    
